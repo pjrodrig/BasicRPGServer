918ee2754f2795b34ef3bb8bc6cca5e0970982ff
Paul Rodrigue <paul.john.rodrigue@gmail.com>
2018-12-15 14:11:16 -0700

    switch from using Client to Pool

--- db.js
+++ db.js
@@ -1,19 +1,11 @@
 const pg = require('pg');
 
 (function() {
-    const db = new pg.Client({
+    const db = new pg.Pool({
         host: 'localhost',
         port: '5432',
         user: 'basicrpgserver',
         database: 'basicrpg'
-    });
-
-    db.connect((err) => {
-        if (err) {
-            console.error('Connection error', err.stack)
-        } else {
-            console.log('Connected')
-        }
     });
 
     db.query('CREATE TABLE IF NOT EXISTS public.rpguser (name citext NOT NULL, id bigserial NOT NULL, PRIMARY KEY (id), CONSTRAINT "uniqueName" UNIQUE (name));');
@@ -21,5 +13,18 @@ const pg = require('pg');
     db.query('CREATE TABLE IF NOT EXISTS public.rpguser_game (user_id bigint NOT NULL, game_id bigint NOT NULL, PRIMARY KEY (user_id, game_id), CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES public.rpguser (id) ON UPDATE NO ACTION ON DELETE CASCADE, CONSTRAINT game_id FOREIGN KEY (game_id) REFERENCES public.game (id) ON UPDATE NO ACTION ON DELETE CASCADE)');
     db.query('CREATE TABLE IF NOT EXISTS public.game_data (data json NOT NULL, id bigint NOT NULL, PRIMARY KEY (id), CONSTRAINT gameId FOREIGN KEY (id) REFERENCES public.game (id) ON UPDATE NO ACTION ON DELETE CASCADE)');
 
-    module.exports = db;
+    module.exports = {
+        query: (text, values, callback) => {
+           pg.connect((err, client, done) => {
+            if (err) {
+                console.error('Connection error', err.stack)
+            } else {
+                client.query(text, values, (err, result) => {
+                    done();
+                    callback(err, result);
+                });
+            }
+           });
+        }
+     };
 })()
